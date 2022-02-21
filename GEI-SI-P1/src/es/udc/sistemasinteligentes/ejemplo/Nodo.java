f50f1feb37c0e926c35fd727859f662f68364f00
nuriacodesido <72859116+nuriacodesido@users.noreply.github.com>
2022-02-21 16:47:29 +0100

    Add files via upload

---
+++ GEI-SI-P1/src/es/udc/sistemasinteligentes/ejemplo/Nodo.java
@@ -0,0 +1,21 @@
+package es.udc.sistemasinteligentes.ejemplo;
+
+
+import es.udc.sistemasinteligentes.Accion;
+import es.udc.sistemasinteligentes.Estado;
+
+public class Nodo {
+
+    private Nodo padre;
+    private Accion accion;
+    private Estado estado;
+
+    public Nodo(Estado estado, Nodo padre, Accion accion){
+        this.estado=estado;
+        this.padre=padre;
+        this.
+    }
+
+
+
+}
