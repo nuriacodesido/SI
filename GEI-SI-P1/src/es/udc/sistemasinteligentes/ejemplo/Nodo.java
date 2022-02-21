d02ef8a0a456ea215309a06646813ee461e1769f
Nuriacodesido <nuria.codesido@udc.es>
2022-02-21 17:03:33 +0100

    nodo

--- GEI-SI-P1/src/es/udc/sistemasinteligentes/ejemplo/Nodo.java
+++ GEI-SI-P1/src/es/udc/sistemasinteligentes/ejemplo/Nodo.java
@@ -10,12 +10,37 @@ public class Nodo {
     private Accion accion;
     private Estado estado;
 
-    public Nodo(Estado estado, Nodo padre, Accion accion){
+    public Nodo(Nodo padre,Estado estado, Accion accion){
         this.estado=estado;
         this.padre=padre;
-        this.
+        this.accion=accion;
     }
 
+    public Nodo(Nodo padre){
+        this(padre,null,null);
+    }
 
+    public Nodo getPadre() {
+        return padre;
+    }
 
+    public void setPadre(Nodo padre) {
+        this.padre = padre;
+    }
+
+    public Accion getAccion() {
+        return accion;
+    }
+
+    public void setAccion(Accion accion) {
+        this.accion = accion;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
 }
